04a6a13f0390db68509a7e0a4d7a0c568d677030
Gustav Behm <me@rootmos.io>
2019-11-29 14:44:42 +0100

    Move initialization code into a dedicated controller process

---
+++ apps/loom/src/controller.erl
@@ -0,0 +1,30 @@
+-module(controller).
+-behaviour(gen_server).
+
+-export([start_link/0]).
+-export([init/1, handle_call/3, handle_cast/2]).
+
+start_link() ->
+    gen_server:start_link({local, ?MODULE}, ?MODULE, [], []).
+
+init(_Args) ->
+    Salt = integer_to_binary(rand:uniform(16#ffffff), 16),
+    {ok, CWD} = file:get_cwd(),
+    Root = filename:join(CWD, io_lib:format("root-~s", [Salt])),
+    error_logger:info_report([{root, Root}]),
+    ok = filelib:ensure_dir(Root),
+    initialize_ar_meta_db(Root),
+    initialize_ar_storage(),
+    {ok, {}}.
+
+handle_call(_Msg, _From, State) -> {noreply, State}.
+handle_cast(_Msg, State) -> {noreply, State}.
+
+initialize_ar_storage() ->
+    ok = ar_storage:start().
+
+initialize_ar_meta_db(Root) ->
+    _Pid = ar_meta_db:start(),
+    ar_meta_db:put(transaction_blacklist_files, []),
+    ar_meta_db:put(content_policy_files, []),
+    ar_meta_db:put(data_dir, filename:join(Root, "data")).
--- apps/loom/src/loom_sup.erl
+++ apps/loom/src/loom_sup.erl
@@ -8,21 +8,17 @@ start_link() ->
     supervisor:start_link({local, ?MODULE}, ?MODULE, []).
 
 init([]) ->
-    initialize_ar_meta_db(),
-    initialize_ar_storage(),
     SupFlags = #{strategy => one_for_all, intensity => 0, period => 1},
-    ChildSpecs = [#{id => bridge,
-                    start => {ar_bridge, start_link, [[[], [], 9000]]},
-                    restart => permanent,
-                    shutdown => 5000
-                   }],
+    ChildSpecs = [
+        #{id => controller,
+          start => {controller, start_link, []},
+          restart => permanent,
+          shutdown => 5000
+         },
+        #{id => bridge,
+          start => {ar_bridge, start_link, [[[], [], 9000]]},
+          restart => permanent,
+          shutdown => 5000
+         }
+    ],
     {ok, {SupFlags, ChildSpecs}}.
-
-initialize_ar_storage() ->
-    ar_storage:start().
-
-initialize_ar_meta_db() ->
-	ar_meta_db:start(),
-	ar_meta_db:put(transaction_blacklist_files, []),
-	ar_meta_db:put(content_policy_files, []),
-	ar_meta_db:put(data_dir, "data").
